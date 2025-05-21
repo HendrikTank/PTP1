dd97fcfb098e8d37b24033bbf45f12b43b56a8e6
HendrikTank <148340589+HendrikTank@users.noreply.github.com>
2025-05-21 11:25:28 +0200

    Update SimplifiedBlackJackAgent.java

--- a2x1/src/simplifiedBlackJack/SimplifiedBlackJackAgent.java
+++ a2x1/src/simplifiedBlackJack/SimplifiedBlackJackAgent.java
@@ -17,62 +17,68 @@ import static cards.Card.Constant.*;
 public class SimplifiedBlackJackAgent {
     
     // Ersetzen Sie diesen Kommentar durch Ihren Code
-    // oder falls kein Code noetig ist, loeschen diesen Kommentar einfach.
-    // Hier waere die Stelle fuer moegliche Zustandsvariablen, Exemplarvariablen
-    // bzw. Objekt-spezifische Variablen oder einen Konstruktor,
-    // sofern derartiges benoetigt wird.
-    // Es ist Ihre Entscheidung und sie sollte Sinn machen.
-    
-    private int AceValue = 11;
-    private int TriValue = 10;
-    
-    /**
-     * "Play" Black Jack
-     */
-    public void playBlackJack(){
-        
-        // create new deck-instance
-    	Deck deck = new Deck();
-    	// instantiate and declare points counter for each run
-        int totalPoints = 0;
-        
-        // Play until at least 17 Points with while loop
-        while (totalPoints < 17) {
-        	// Deal new card and store it in local variable
-            Card drawnCard = deck.deal();// todo: karte direkt ausgeben 
-            System.out.printf("%s",drawnCard);
-            // get Cards Rank (e.g. CA, H9, etc)
-            final Rank rank = drawnCard.getRank();
-            //declare new local variable but don't instantiate it yet
-            int cardValue;
-            
-            // Compare Card Value and instantiate cardValue with the corresponding value
-            if (rank == Rank.ACE) {
-                cardValue = AceValue; //ACE-Value is 11
-            } else if (rank == Rank.JACK || rank == Rank.QUEEN || rank == Rank.KING) {
-                cardValue = TriValue; //Non-Ace special card value is 10
-            } else {
-                cardValue = rank.value(); // fallback for non-speical cards to get Card Value with rank.value() method
-            }// maybe simplify to switch case later?
-            
-            
-            totalPoints += cardValue; //Increment points by current cards value totalPoint = totalPoints + cardValue
-            
-            // Print Point and Card Type
-            System.out.printf("Punkte: %s ",totalPoints);
-            System.out.printf("(Diff: %s)\n",cardValue);
+// oder falls kein Code noetig ist, loeschen diesen Kommentar einfach.
+// Hier waere die Stelle fuer moegliche Zustandsvariablen, Exemplarvariablen
+// bzw. Objekt-spezifische Variablen oder einen Konstruktor,
+// sofern derartiges benoetigt wird.
+// Es ist Ihre Entscheidung und sie sollte Sinn machen.
 
-        }
-        if (totalPoints > 21) {
-            System.out.println("LOST");
+private int AceValue = 11;
+private int TriValue = 10;
+
+/**
+ * "Play" Black Jack
+ */
+public void playBlackJack(){
+    
+    // create new deck-instance
+	Deck deck = new Deck();
+	// instantiate and declare points counter for each run
+    int totalPoints = 0;
+    
+    // Play until at least 17 Points with while loop
+    while (totalPoints < 17) {
+    	// Deal new card and store it in local variable
+        Card drawnCard = deck.deal();// todo: karte direkt ausgeben 
+        System.out.printf("%s",drawnCard);
+        // get Cards Rank (e.g. CA, H9, etc)
+        final Rank rank = drawnCard.getRank();
+        //declare new local variable but don't instantiate it yet
+        int cardValue;
+        
+        // Compare Card Value and instantiate cardValue with the corresponding value
+        switch (rank){
+            case ACE:
+                cardValue = AceValue; //ACE-Value is 11
+                break;
+            case JACK, QUEEN, KING:
+                cardValue = TriValue; //Non-Ace special card value is 10
+                break;
+            default:
+                cardValue = rank.value(); // fallback for non-speical cards to get Card Value with rank.value() method
+                break; // unnötig, aber nicht falsch
         }
         
+        totalPoints += cardValue; //Increment points by current cards value totalPoint = totalPoints + cardValue
         
-        //^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-        // Sofern Sie kein Vorwissen haben und/oder NICHT wissen was Sie tun
-        // führen Sie KEINE! Änderungen unterhalb dieser Zeilen durch.
-        //^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-        System.out.flush();
-    }//method()
+        // Print Point and Card Type
+        System.out.printf("Punkte: %s (Diff: %s)\n",totalPoints,cardValue);
+
+    }
+    if (totalPoints > 21) {
+        System.out.println("LOST");
+    }
+
+    if (totalPoints == 21) {
+        System.out.println("WIN");
+    }
+    
+    
+    //^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+    // Sofern Sie kein Vorwissen haben und/oder NICHT wissen was Sie tun
+    // führen Sie KEINE! Änderungen unterhalb dieser Zeilen durch.
+    //^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+    System.out.flush();
+}//method()
     
 }//class
