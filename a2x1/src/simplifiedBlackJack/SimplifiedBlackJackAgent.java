0cd6138237ee5a53040ff74f613f1af53ce06c00
HendrikTank <148340589+HendrikTank@users.noreply.github.com>
2025-05-21 10:50:21 +0200

    Update SimplifiedBlackJackAgent.java

--- a2x1/src/simplifiedBlackJack/SimplifiedBlackJackAgent.java
+++ a2x1/src/simplifiedBlackJack/SimplifiedBlackJackAgent.java
@@ -11,8 +11,8 @@ import static cards.Card.Constant.*;
 /**
  * Simplified Black Jack Agent - see task
  * 
- * @author  (your name(s)) 
- * @version (a version number or a date)
+ * @author  Hendrik, Christoph Tank 
+ * @version v1.0.0
  */
 public class SimplifiedBlackJackAgent {
     
@@ -23,31 +23,15 @@ public class SimplifiedBlackJackAgent {
     // sofern derartiges benoetigt wird.
     // Es ist Ihre Entscheidung und sie sollte Sinn machen.
     
-    public int AceValue = 11;
-    public int TriValue = 10;
+    private int AceValue = 11;
+    private int TriValue = 10;
     
     /**
      * "Play" Black Jack
      */
     public void playBlackJack(){
-        //VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV
-        //###
-        //###
-        //###
-        //###           HIER kommt Ihr Code hin
-        //###
-        //###                    VVVV
-        //###                    VVVV
-        //###                    VVVV
-        //###   VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV
-        //###      VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV
-        //###         VVVVVVVVVVVVVVVVVVVVVVVVVV
-        //###            VVVVVVVVVVVVVVVVVVVV
-        //###               VVVVVVVVVVVVVV
-        //###                  VVVVVVVV
-        //###                     VV
         
-        // create new deck instance
+        // create new deck-instance
     	Deck deck = new Deck();
     	// instantiate and declare points counter for each run
         int totalPoints = 0;
@@ -55,9 +39,10 @@ public class SimplifiedBlackJackAgent {
         // Play until at least 17 Points with while loop
         while (totalPoints < 17) {
         	// Deal new card and store it in local variable
-            Card drawnCard = deck.deal();
+            Card drawnCard = deck.deal();// todo: karte direkt ausgeben 
+            System.out.printf("%s",drawnCard);
             // get Cards Rank (e.g. CA, H9, etc)
-            Rank rank = drawnCard.getRank();
+            final Rank rank = drawnCard.getRank();
             //declare new local variable but don't instantiate it yet
             int cardValue;
             
@@ -68,22 +53,18 @@ public class SimplifiedBlackJackAgent {
                 cardValue = TriValue; //Non-Ace special card value is 10
             } else {
                 cardValue = rank.value(); // fallback for non-speical cards to get Card Value with rank.value() method
-            }
+            }// maybe simplify to switch case later?
             
             
-            totalPoints += cardValue; //Increment points by current cards value
+            totalPoints += cardValue; //Increment points by current cards value totalPoint = totalPoints + cardValue
             
             // Print Point and Card Type
-            System.out.println(drawnCard + " Punkte: " + totalPoints);
-            
-            // Escape loop if Points are above 21 and prints lost state to console
-            if (totalPoints > 21) {
-                System.out.println("LOST");
-                break;
-            }
+            System.out.printf("Punkte: %s ",totalPoints);
+            System.out.printf("(Diff: %s)\n",cardValue);
+
         }
-        if (totalPoints <= 21) {
-        	System.out.println("WON");
+        if (totalPoints > 21) {
+            System.out.println("LOST");
         }
         
         
